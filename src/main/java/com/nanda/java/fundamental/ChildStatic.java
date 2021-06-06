1813ccf15284bb25a2e73f4fdda8f304ac7e1a4f
nrangasa <nandakumar.rangasamy@oracle.com>
2021-06-06 13:25:53 +0530

    updated

---
+++ src/main/java/com/nanda/java/fundamental/ChildStatic.java
@@ -0,0 +1,8 @@
+package com.nanda.java.fundamental;
+
+public class ChildStatic extends StaticTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/nanda/java/fundamental/StaticTest.java
@@ -0,0 +1,27 @@
+package com.nanda.java.fundamental;
+
+public class StaticTest {
+
+    int instanceVariable;
+    static {
+        System.out.println("static block");
+
+    }
+
+    public StaticTest() {
+        System.out.println("constructor");
+        this.instanceVariable = 10;
+    }
+
+    public void printVariables() {
+        System.out.println(this.instanceVariable);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("main");
+        new StaticTest().printVariables();
+        System.out.println("hello");
+    }
+}
